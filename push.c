fd90098fc142b0b0295c052169b4e459ef84c44a
Richie911 <oohrichmond.k@gmail.com>
2023-10-18 16:50:08 -0700

    fix

--- push.c
+++ push.c
@@ -7,31 +7,42 @@
 */
 void _push(stack_t **head, unsigned int counter)
 {
-	int i, j = 0, flag = 0;
+	int n, j = 0, flag = 0;
 
 	if (bus.arg)
 	{
 		if (bus.arg[0] == '-')
 			j++;
-		for (; bus.arg[j] != '\0'; j++)
+
+		while (bus.arg[j] != '\0')
 		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1; }
+			if (bus.arg[j] > '9' || bus.arg[j] < '0')
+				flag = 1;
+			j++;
+		}
+
 		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
-			exit(EXIT_FAILURE); }}
+			exit(EXIT_FAILURE);
+		}
+	}
 	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	i = atoi(bus.arg);
+		exit(EXIT_FAILURE);
+	}
+
+	n = atoi(bus.arg);
+
 	if (bus.lifi == 0)
-		addnode(head, i);
+		addnode(head, n);
 	else
-		addqueue(head, i);
+		addqueue(head, n);
 }
