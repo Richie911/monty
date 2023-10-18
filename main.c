171b31985c03c86578277ef87bf216406d854c0c
Richie911 <oohrichmond.k@gmail.com>
2023-10-18 16:54:06 -0700

    fix

--- main.c
+++ main.c
@@ -9,35 +9,38 @@ bus_t bus = {NULL, NULL, NULL, 0};
 
 int main(int argc, char *argv[])
 {
-	FILE *file_path;
-	ssize_t line = 1;
-	size_t line_buf_size = 0;
-	char *line_buf;
-	int i;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file_path = fopen(argv[1], "r");
-	bus.file = file_path;
-	if (file_path == NULL)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (line > 0)
+	while (read_line > 0)
 	{
-		line = getline(&line_buf, &line_buf_size, file_path);
-		bus.content = line_buf;
-		i++;
-		if (line > 0)
-			execute(line_buf, &stack, i, file_path);
-		free(line_buf);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
 	free_stack(stack);
-	fclose(file_path);
-	return (0);
+	fclose(file);
+return (0);
 }
