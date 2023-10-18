b117d92015f65f9120f65d8cdc2775750c43cb0b
Richie911 <oohrichmond.k@gmail.com>
2023-10-18 16:20:19 -0700

    fix

--- execute.c
+++ execute.c
@@ -12,21 +12,11 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	int i;
 	char *operator;
 	instruction_t funcs[] = {
-				{"push", _push},
-				{"pall", _pall},
-				{"pint", _pint},
-				{"pop", _pop},
-				{"swap", _swap},
-				{"add", _add},
-				{"nop", _nop},
-				{"sub", f_sub},
-				{"div", f_div},
-				{"mul", f_mul},
-				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
+				{"push", _push}, {"pall", _pall}, {"pint", _pint},
+				{"pop", _pop}, {"swap", _swap}, {"add", _add},
+				{"nop", _nop}, {"sub", f_sub}, {"div", f_div},
+				{"mul", f_mul}, {"mod", f_mod}, {"pchar", f_pchar},
+				{"pstr", f_pstr}, {"rotl", f_rotl}, {"rotr", f_rotr},
 				{"queue", f_queue},
 				{"stack", f_stack},
 				{NULL, NULL}
